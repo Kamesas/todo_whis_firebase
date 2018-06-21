d2023b29bad62ec309caa14f14816600db76127f
Alexandr <semakaleksandr2014@gmail.com>
2018-06-21 09:17:07 +0300

    removeNote() is ready

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   constructor(props){
     super(props);
       this.addNote = this.addNote.bind(this);
+      this.removeNote = this.removeNote.bind(this);
 
       this.app = firebase.initializeApp(DB_CONFIG);
       this.database = this.app.database().ref().child('notes');
@@ -30,6 +31,19 @@ class App extends Component {
         id: snap.key,
         noteContent: snap.val().noteContent,
       })
+
+      this.setState({
+        notes: previosNotes
+      })
+    })
+
+    this.database.on("child_removed", snap => {
+      for(var i=0; i < previosNotes.length; i++){
+        if(previosNotes[i].id === snap.key){
+          previosNotes.splice(i, 1);
+        }
+      }
+
       this.setState({
         notes: previosNotes
       })
@@ -39,6 +53,10 @@ class App extends Component {
 
   addNote(note){
     this.database.push().set({noteContent: note});
+  }
+
+  removeNote(noteId){
+    this.database.child(noteId).remove();
   }
 
   render() {
@@ -51,7 +69,7 @@ class App extends Component {
           {
             this.state.notes.map((note, ) => {
               return (
-                <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+                <Note noteContent={note.noteContent} noteId={note.id} key={note.id} removeNote = {this.removeNote} />
               )              
             })
             
